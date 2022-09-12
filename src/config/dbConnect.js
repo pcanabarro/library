523700349399cf6c60b9344109093124c537d746
Pedro Canabarro <101909918+pcanabarro@users.noreply.github.com>
2022-09-12 15:16:54 -0300

    Update dbConnect.js

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://pedro:123@library.oyuse8k.mongodb.net/library") //this string creates a DataBase || when the database is wrong, the array is empty
+mongoose.connect("mongodb+srv://0x0x0x:0x0x0x@library.oyuse8k.mongodb.net/library") //this string creates a DataBase || when the database is wrong, the array is empty
 
 let db = mongoose.connection
 
